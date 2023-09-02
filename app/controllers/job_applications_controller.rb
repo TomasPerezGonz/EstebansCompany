0d5216143c73d9bfc9f5f0102178bfbf7683f381
Tomas <tomasperezg@gmail.com>
2023-09-01 21:14:48 -0400

    Implemented notifications when a user applies for a job

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,30 +1,18 @@
 class JobApplicationsController < ApplicationController
 
     def index
-        @job_applications = current_user.job_applications
+      @job_applications = current_user.job_applications
     end
-
+  
     def create
-        @job_application = JobApplication.new(user_id: current_user.id, job_offer_id: params[:job_offer_id])
-        if @job_application.save
-         
-          redirect_to job_offers_path, notice: 'Postulación exitosa.'
-        else
-          redirect_to job_offers_path, alert: 'Error al postular.'
-        end
+      @job_application = JobApplication.new(user_id: current_user.id, job_offer_id: params[:job_offer_id])
+      if @job_application.save
+        esteban = User.find_by(email: 'esteban@a.a')
+        Notification.create(user: esteban, content: 'Nueva postulación recibida', read: false)
+        redirect_to job_offers_path, notice: 'Postulación exitosa.'
+      else
+        redirect_to job_offers_path, alert: 'Error al postular.'
       end
-      
-      def create
-        @job_application = JobApplication.new(user_id: current_user.id, job_offer_id: params[:job_offer_id])
-        if @job_application.save
-          
-          esteban = User.find_by(email: 'esteban@a.a')
-          Notification.create(user: esteban, content: 'Nueva postulación recibida', read: false)
-          
-          redirect_to job_offers_path, notice: 'Postulación exitosa.'
-        else
-          redirect_to job_offers_path, alert: 'Error al postular.'
-        end
-      end
-      
-end
+    end
+  
+  end
