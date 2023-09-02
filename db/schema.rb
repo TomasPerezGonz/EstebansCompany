9779d25a0e2ed1cc1f8c218534ffdab72e010667
Tomas <tomasperezg@gmail.com>
2023-09-01 20:27:04 -0400

    Generated Application model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_002604) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_002653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "job_offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_offer_id"], name: "index_applications_on_job_offer_id"
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "job_offers", force: :cascade do |t|
     t.string "title"
@@ -31,5 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_002604) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "applications", "job_offers"
+  add_foreign_key "applications", "users"
   add_foreign_key "job_offers", "users"
 end
