9e9d9251d2ad72923cee7baf21a6eb41f73b6c9e
Tomas <tomasperezg@gmail.com>
2023-09-01 20:26:15 -0400

    Generated JobOffer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_002510) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_002604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "job_offers", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_job_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_002510) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "job_offers", "users"
 end
