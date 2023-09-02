dce85d7dc5db330112f9ac845e3caf2309a0ec60
Tomas <tomasperezg@gmail.com>
2023-09-01 21:01:03 -0400

    Implemented internal notifications for Esteban on new job applications

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -14,6 +14,9 @@ class JobApplicationsController < ApplicationController
         @job_application = JobApplication.new(user_id: current_user.id, job_offer_id: params[:job_offer_id])
         if @job_application.save
           
+          esteban = User.find_by(email: 'esteban@a.a')
+          Notification.create(user: esteban, content: 'Nueva postulación recibida', read: false)
+          
           redirect_to job_offers_path, notice: 'Postulación exitosa.'
         else
           redirect_to job_offers_path, alert: 'Error al postular.'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     has_many :job_offers
     has_many :applications
+    has_many :notifications, dependent: :destroy
+
 
 end
