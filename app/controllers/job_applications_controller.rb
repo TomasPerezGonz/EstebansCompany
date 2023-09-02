9c402419630670209a129ff553e3d5cd4d1be0a2
Tomas <tomasperezg@gmail.com>
2023-09-01 20:55:53 -0400

    Added profile view to display user applications

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -10,4 +10,14 @@ class JobApplicationsController < ApplicationController
         end
       end
       
+      def create
+        @job_application = JobApplication.new(user_id: current_user.id, job_offer_id: params[:job_offer_id])
+        if @job_application.save
+          
+          redirect_to job_offers_path, notice: 'Postulación exitosa.'
+        else
+          redirect_to job_offers_path, alert: 'Error al postular.'
+        end
+      end
+      
 end
